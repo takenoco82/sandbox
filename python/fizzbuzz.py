9587366ad05064dac76c70d50908e06f7404ec8c
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-08-05 17:11:01 +0900

    [Add] FizzBuzzを作成

---
+++ python/fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz(n):
+    return n
+
+
+def main():
+    for n in range(1, 11):
+        print(fizzbuzz(n))
+
+
+def funcname(parameter_list):
+    pass
+
+
+if __name__ == '__main__':
+    main()
