b823d3fa1dba794cfa252d000494fbde78d6e409
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-08-07 04:23:08 +0900

    [Add] 引数チェックを実装

--- python/app/fizzbuzz.py
+++ python/app/fizzbuzz.py
@@ -1,4 +1,9 @@
+from .IllegalArgmentError import IllegalArgmentError
+
+
 def fizzbuzz(n):
+    validate(n)
+
     if n % 3 == 0 and n % 5 == 0:
         return 'fizzbuzz'
     elif n % 3 == 0:
@@ -6,3 +11,14 @@ def fizzbuzz(n):
     elif n % 5 == 0:
         return 'buzz'
     return str(n)
+
+
+def validate(n):
+    if n is None:
+        raise IllegalArgmentError
+    if n is '':
+        raise IllegalArgmentError
+    if type(n) is not int:
+        raise IllegalArgmentError
+    if n < 0:
+        raise IllegalArgmentError
